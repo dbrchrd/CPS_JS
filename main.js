41ee8c7a453e35af74ef368abf3866471c147106
dbrchrd <83125728+dbrchrd@users.noreply.github.com>
2022-03-05 17:53:39 +0100

    Update main.js
    Replace all old syntax (for querySelector)

--- main.js
+++ main.js
@@ -2,13 +2,13 @@ console.log('main.js is running');
 const qs = (elPath) => {
   return document.querySelector(elPath);
 }
-const btn_start = document.querySelector("#btn-start"), btn_click = document.querySelector("#btn-click"), btn_fin = document.querySelector("#btn-fin"), btn_retry = document.querySelector("#retry");
+const btn_start = qs("#btn-start"), btn_click = qs("#btn-click"), btn_fin = qs("#btn-fin"), btn_retry = qs("#retry");
 
 var btn_info, btn_info_innerHTML, timer_duration, append_seconds = timer_duration, append_tens = 00, count = 0, tens;
 const start = () => {
   Interval = setInterval(timer, 10);
   count++;
-  timer_duration = document.querySelector("#timer-input").value;
+  timer_duration = qs("#timer-input").value;
   btn_info_innerHTML = "Click";
   append_seconds = timer_duration;
   console.log("Timer duration : " + timer_duration + " seconds");
@@ -35,15 +35,15 @@ btn_retry.onclick = () => {
 }
 const timer = () => {
 
-  btn_info = document.querySelector(".btn-info");
-  btn_info_innerHTML = document.querySelector(".btn-info").innerHTML;
+  btn_info = qs(".btn-info");
+  btn_info_innerHTML = qs(".btn-info").innerHTML;
   append_tens = append_tens - 1;
 
-  document.querySelector("#sec").innerHTML = append_seconds;
-  document.querySelector("#tens").innerHTML = append_tens;
+  qs("#sec").innerHTML = append_seconds;
+  qs("#tens").innerHTML = append_tens;
 
   if (append_tens < 10) {
-    document.querySelector("#tens").innerHTML = "0" + append_tens;
+    qs("#tens").innerHTML = "0" + append_tens;
   }
   if (append_tens < 1) {
     append_seconds = append_seconds - 1;
@@ -58,18 +58,18 @@ const timer = () => {
     btn_click.style.display = "none";
     btn_fin.style.display = "flex";
     if (count < 2) {
-      document.querySelector(".some-clicks").innerHTML = "";
+      qs(".some-clicks").innerHTML = "";
     } else {
-      document.querySelector(".some-clicks").innerHTML = "s";
+      qs(".some-clicks").innerHTML = "s";
     }
-    document.querySelector("#timer").innerHTML = "Finished !";
-    document.querySelector("#fin-clicks-display").innerHTML = count;
-    document.querySelector("#fin-time-display").innerHTML = timer_duration + " s";
-    document.querySelector("#fin-cps-display").innerHTML = (count / timer_duration).toFixed(2);
+    qs("#timer").innerHTML = "Finished !";
+    qs("#fin-clicks-display").innerHTML = count;
+    qs("#fin-time-display").innerHTML = timer_duration + " s";
+    qs("#fin-cps-display").innerHTML = (count / timer_duration).toFixed(2);
     count = 0;
-    tens = document.querySelector("#tens").innerText;
+    tens = qs("#tens").innerText;
     if (tens == "0-1") {
-      document.querySelector("#tens").innerText = "00";
+      qs("#tens").innerText = "00";
     }
   }
 
