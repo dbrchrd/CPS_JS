413eeeda2e073b0702756f144bda9ec78d166e53
dbrchrd <83125728+dbrchrd@users.noreply.github.com>
2022-03-13 19:22:00 +0100

    Update main.js
    Listeners for the input

--- main.js
+++ main.js
@@ -8,3 +8,12 @@ const show = (bool, elPath) => {
   return elPath, bool;
 }
 const btn_start = qs("#btn-start"), btn_click = qs("#btn-click"), btn_fin = qs("#btn-fin"), btn_retry = qs("#retry");
+
+/*************** Listeners ***************/
+qs(".aside > input").onclick = () => {
+  qs(".aside > input").value = "";
+}
+qs(".aside > input").oninput = () => {
+  qs(".aside > input").value = qs(".aside > input").value.replace(/[^0-9.]/g, '').replace(/(\..*)\./g, '$1');
+  qs("#secs").innerText = qs(".aside > input").value;
+}
